55206f65caa0c4ceb73907d2045f8cabdc0fabda
OlimpiaP <olimpia.parje@gmail.com>
2016-04-14 23:04:45 +0200

    Added files via upload

---
+++ index.R
@@ -0,0 +1,146 @@
+---
+output: 
+  html_document: 
+    theme: cosmo
+authors: Olimpia Parje
+---
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+background-color:#4CAF50;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="#Presentation">Top</a></li>  
+  <li><a href="#top">Top</a></li>
+  <li><a href="#world">Corruption in the World</a></li>
+  <li><a href="#findings">Findings</a></li>
+  <li><a href="#interactive">Tools</a></li>
+  <li><a href="#contact">Contact us</a></li>
+</ul>
+
+</body>
+</html>
+
+# Set working directory
+getwd()
+try(setwd("/Users/Olimpia/Desktop/Data-collection"), silent = TRUE)
+
+# Load packages
+library(markdown)
+
+
+<center><h1>Who advises the presidents</h1></center>
+
+<center><h2> The Role of the President in romania 21st Century</h2></center>
+
+ (*) The President is the head of state and is in charge of national security and external relations
+ (*) Additionally, the president appoints the prime-minister (head of government), normally from the party (or coalition of parties) that has the majority in the Parliament; approves Ministerial appointments
+ (*) The President has the role of the mediator between the state and society
+ (*) Can send back laws to Parliament (twice); appoints judges;
+
+
+
+<center><h3>Romania's presidents<center><h3>
+:-------------------------:|:-------------------------:
+<img src="who.png" width="540px" height="540px" /> 
+
+
+
+<a href="#top">Go to top</a>
+
+<a name="world"><center><b><h3>All the president's men </h3></b></center></a>
+
+<center><h3>Who advises the president<h3>
+
+Advisers are appointed by each president with discretionary power. 
+There are two types of advisors:
+ (*) Presidential advisers (Have the rank of Minister)
+ (*) State advisers (Have the rank of State Secretary)
+
+
+<center>Place the cursor over a president's term to see how many and which type of advisors were in their service </center>
+
+<script id="infogram_0_UKa6Mx6rZ9ea9JBw" title="All the president's men" src="//e.infogr.am/js/embed.js?TIF" type="text/javascript"></script><div style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px"><a target="_blank" href="https://infogr.am/UKa6Mx6rZ9ea9JBw" style="color:#989898!important;text-decoration:none!important;">All the president's men</a><br><a style="color:#989898!important;text-decoration:none!important;" href="https://infogr.am" target="_blank">Create your own infographics</a></div>
+
+
+Observations:
+
+(1) Since the formation of the modern Romanian republic we have seen a steep increase in the number of presidential advisers, from 7 to a peak of 45 during Traian Basescu's first term
+(2) Aditonally, an increase in the type of advisors, moreso, an increase in the State Advisers (which are the rank of State Secretary, one step below Presidential advisers), plus the creation of additional 'Personal adviser' roles.
+(3) This underlines the stronger need for personal loyalties, potentially need for more political support.
+
+
+<br>
+<br>
+<a name="ourwork"><center><h3>What we did</h3></center></a>
+
+We gathered data from the World Bank ([World Development Indicators](http://data.worldbank.org/data-catalog/world-development-indicators) and [World Governance Indicators](http://info.worldbank.org/governance/wgi/index.aspx#home)) in order to answer the following research question: 
+
+<center><b>Are countries with a higher dependence on natural resources more corrupt?</b></center>
+<br>
+To answer this question, we built a model in which corruption (our dependent variable) is determined by three explanatory variables: 
+<br>
+<br>
+
+Natural 
+
+<br>
+**Hypothesis**: We assum  
+
+**Note**: 
+
+
+<a name="interactive"><center><h3>Interactive Data Analysis</h3></center></a>
+
+This interactive tool will allow you to see some interesting changes in the way the President's advice is and has been organized in Romania 
+
+**How to use it:**
+
+
+
+
+
+<a name="findings"><center><h3>Overall trends and observations </h3></center></a>
+
+(1) The dependence o
+(2) The more depende
+(3) The level of inc
+(4) We could not fin
+(5) There are many o
+All in all, we canno
+
+
+<a name="contact"><h3>Contact information</h3></a>
+
+Write to [Olimpia Parje](mailto: olimpia.parje@gmail.com(olimpia.parje@gmail.com)) 
+
